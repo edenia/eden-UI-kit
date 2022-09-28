a9844946ec59613a7a7f38744afbabfb946d7f2f
Angelo CG <angelocas13_8@hotmail.com>
2022-09-28 14:21:47 -0600

    feat(components): create ActionItemDelegateBp component

---
+++ src/components/ActionItemDelegateBp/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ActionItemDelegateBp = ({ headItem, text, icon, link, target }) => {
+  return (
+    <div>
+      <div className="alignCenter">{headItem}</div>
+      <div className="centerItems">
+        <span className="body1">{text}</span>
+        <a className="linkPadding" href={link} target={target}>
+          <img src={icon} alt="action item icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+ActionItemDelegateBp.propTypes = {
+  headItem: PropTypes.node,
+  target: PropTypes.string,
+  text: PropTypes.string,
+  icon: PropTypes.string,
+  link: PropTypes.string,
+};
+
+export default ActionItemDelegateBp;

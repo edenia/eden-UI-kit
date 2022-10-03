f2eee2b67cbd8c81eba06ad631eb01af2a118a0f
Angelo CG <angelocas13_8@hotmail.com>
2022-10-03 10:28:40 -0600

    feat(components): create sidebar mobile version

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,24 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SideBar = ({ logo, menuOptions, profileComponent }) => {
+const SideBar = ({ open, logo, close, menuOptions, profileComponent }) => {
+  if (!open) return;
+
   return (
-    <div className="sidebar-container">
-      <div className="sidebar-body-head-container">
-        <div className="sidebar-logo-container">
-          <img src={logo} alt="App logo" />
+    <div className="sidebar-drawer-mobile">
+      <div className="sidebar-mobile-background" onClick={close} />
+      <div className="sidebar-container">
+        <div className="sidebar-body-head-container">
+          <div className="sidebar-logo-container">
+            <img src={logo} alt="App logo" />
+          </div>
+          <div className="sidebar-options-container">{menuOptions}</div>
         </div>
-        <div className="sidebar-options-container">{menuOptions}</div>
+        <div className="sidebar-padding-profile">{profileComponent}</div>
       </div>
-      <div className="sidebar-padding-profile">{profileComponent}</div>
     </div>
   );
 };
 
 SideBar.propTypes = {
+  open: PropTypes.func,
+  close: PropTypes.func,
   logo: PropTypes.string,
   menuOptions: PropTypes.node,
   profileComponent: PropTypes.node,
+};
+
+SideBar.defaultProps = {
+  open: true,
 };
 
 export default SideBar;
